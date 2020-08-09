a3bb7bad60372aed9099e1156ec555774c2ce2b5
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-08-09 20:29:39 -0300

    componentização de inputs: Inoutr inputs: Input, Select e TextArea

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,26 +2,18 @@ import React from 'react'
 import PageHeader from '../../components/PageHeader'
 import './styles.css'
 import TeacherItem from '../../components/TeacherItem'
+import Input from '../../components/Input'
 
 export default function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis: ">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
 
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da semana</label>
-                        <input type="text" id="week-day" />
-                    </div>
+                    <Input name="subject" label="Matéria" />
+                    <Input name="week-day" label="Dia da Semana" />
+                    <Input name="time" label="Hora" />
 
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
                 </form>
             </PageHeader>
             <main>
