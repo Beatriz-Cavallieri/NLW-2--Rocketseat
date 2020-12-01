4887a56da670b5ae17dbe6bc08336acda3b34942
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-12-01 19:57:56 -0300

    Apagar para recomeçar do zero.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Routes from './routes'
-import './assets/styles/global.css';
 
 function App() {
-  return <Routes />
+  return <h1>App</h1> 
 }
 
 export default App;
