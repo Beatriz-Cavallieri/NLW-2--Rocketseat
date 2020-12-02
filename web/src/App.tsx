8df1f2c616726e0a092585ba160a94ee23de6026
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-12-01 21:37:10 -0300

    Criar rotas principais

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Router } from 'react-router';
 import './assets/styles/global.css';
 import Landing from './pages/Landing';
+import Routes from './routes';
 
 function App() {
   return (
-    <Landing></Landing>
+    <Routes />
   )
 }
 
