2e73850b972d0ff5db1817d3d07fdd9f27a2b689
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-12-07 23:14:11 -0300

    Adicionar rotas de criar e listar conexoes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
-import express from 'express'
-import routes from './routes'
+import express from "express";
+import routes from "./routes";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(cors());
 
-app.use(routes)
+app.use(express.json());
 
-app.listen(3333)
+app.use(routes);
 
+app.listen(3333);
