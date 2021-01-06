4513eb71f1854c816289f0a7fd49c07a15aa31c8
Beatriz Cavallieri <bfcavallieri@gmail.com>
2021-01-06 00:48:42 -0300

    Web - Landing page: exibir total de conexoes

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
@@ -6,8 +6,16 @@ import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 import "./styles.css";
+import api from "../../services/api";
 
 export default function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      const { total } = response.data;
+      setTotalConnections(total);
+    });
+  }, [totalConnections]);
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -27,7 +35,7 @@ export default function Landing() {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="" />
         </span>
       </div>
